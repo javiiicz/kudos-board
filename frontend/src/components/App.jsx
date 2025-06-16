c78d1724f83c08ecdba94163dfea43e7c2daa85a
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 15:46:58 -0700

    [MVP] [Frontend] Add all components to App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import '../styles/App.css'
+import Footer from './Footer'
+import BoardDisplay from './BoardDisplay'
+import Header from './Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header/>
+      <main>
+        <BoardDisplay/>
+      </main>
+      <Footer/>
     </>
   )
 }
