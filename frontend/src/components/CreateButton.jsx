a79297450d609c7e3a03665c124115bd9bacbb13
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 16:31:59 -0700

    [MVP] [Frontend] Add Button component

---
+++ frontend/src/components/CreateButton.jsx
@@ -0,0 +1,11 @@
+import "../styles/CreateButton.css"
+
+const CreateButton = () => {
+    return (
+        <button className="create-button">
+            <p>+ Create a New Board</p>
+        </button>
+    )
+}
+
+export default CreateButton;
