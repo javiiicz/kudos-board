5e60503b7c1fd2ec2189cea941c3f0a8d596794b
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 15:46:14 -0700

    [MVP] [Frontend] Create Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/Footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className="footer-text">Made by Javier Carrillo 2025©</p>
+        </footer>
+    )
+}
+
+export default Footer;
