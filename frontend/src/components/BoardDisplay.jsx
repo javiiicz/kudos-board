91e0067b26acdf1a3bbdc8afaed8853932e634c0
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 16:08:36 -0700

    [MVP] [Frontend] Add Board display styling

--- frontend/src/components/BoardDisplay.jsx
+++ frontend/src/components/BoardDisplay.jsx
@@ -1,8 +1,26 @@
 import "../styles/BoardDisplay.css"
+import Board from "./Board"
+
+let placeholderBoard = {title: 'School Board', image: 'https://placehold.co/600x400', author: 'Javier Carrillo', desc: "hello this is a placeholder"}
 
 const BoardDisplay = () => {
     return (
-        <></>
+        <div className="board-container">
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+            <Board board={placeholderBoard}/>
+        </div>
     )
 }
 
