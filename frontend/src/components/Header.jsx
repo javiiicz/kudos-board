38309614135d6dee2e4b14feec52b519ca40c920
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 16:32:41 -0700

    [MVP] [Frontend] Implement components to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import "../styles/Header.css"
+import Search from "./Search";
+import Filters from "./Filters";
+import CreateButton from "./CreateButton";
 
 const Header = () => {
     return (
         <header>
             <h1>Kudos Board!</h1>
-            
+            <Search/>
+            <Filters/>
+            <CreateButton/>
         </header>
     )
 }
