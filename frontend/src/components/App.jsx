075c87f681b7e87a4bf4b835f1e04cfffa639865
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 17:07:51 -0700

    [MVP] [Frontend] Refactor Pages

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,13 +3,15 @@ import "../styles/App.css";
 import Footer from "./Footer";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage.jsx";
+import BoardPage from "./BoardPage.jsx";
 
 function App() {
     return (
         <>
-        <Routes>
-            <Route path="/" element={<HomePage/>}/>
-        </Routes>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/boards/:id" element={<BoardPage />} />
+            </Routes>
             <Footer />
         </>
     );
