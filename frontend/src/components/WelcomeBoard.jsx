34224776a63e02b6ccf7793b10f4b28da244a9e4
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-17 16:06:56 -0700

    [MVP] [Frontend] Fix typo

--- frontend/src/components/WelcomeBoard.jsx
+++ frontend/src/components/WelcomeBoard.jsx
@@ -1,7 +1,7 @@
 const WelcomeBoard = () => {
     return (
         <div className="board">
-            <h2>"Welcome to Kudomate!"</h2>
+            <h2>Welcome to Kudomate!</h2>
             <img src="./snoopy.gif" alt="Welcome board image"></img>
             <h3>Javi</h3>
             <p>To start, press the create board button to the left!</p>
