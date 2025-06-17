ed5ae4eff178293af00d2adf0bae73fff0307819
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 17:17:17 -0700

    [MVP] [Frontend] Edit footer text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "../styles/Footer.css"
 const Footer = () => {
     return (
         <footer>
-            <p className="footer-text">Made by Javier Carrillo 2025©</p>
+            <p className="footer-text">Made by Javier Carrillo</p>
         </footer>
     )
 }
