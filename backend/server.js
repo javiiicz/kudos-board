d2670d727c82a932a30e5b89708b115e6f0c8ed9
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-17 14:45:42 -0700

    [MVP] [Backend] Add error handling endpoint and catch-all

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,16 @@ server.get("/", (req, res) => {
     res.send("Hello from Express!");
 });
 
-module.exports = server
+// [CATCH-ALL]
+server.use((req, res, next) => {
+    next({ status: 404, message: "Not found" });
+});
+
+// Error handling
+server.use((err, req, res, next) => {
+    const { message, status = 500 } = err;
+    console.error(message);
+    res.status(status).json({ message }); // Unsafe in prod
+});
+
+module.exports = server;
