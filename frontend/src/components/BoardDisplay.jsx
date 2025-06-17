7c21ba90194e0562cf74604a8dd79a15fdf41574
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 22:17:19 -0700

    [MVP] [Frontend] Add board to display when user has no boards

--- frontend/src/components/BoardDisplay.jsx
+++ frontend/src/components/BoardDisplay.jsx
@@ -1,14 +1,34 @@
-import "../styles/BoardDisplay.css"
-import Board from "./Board"
+import "../styles/BoardDisplay.css";
+import Board from "./Board";
+import WelcomeBoard from "./WelcomeBoard";
 
-let placeholderList = [{id: 1, title: 'School Board', image: 'https://placehold.co/600x400', author: 'Javier Carrillo', desc: "hello this is a placeholder"}, {id: 2, title: 'Congrats', image: 'https://placehold.co/600x400', author: 'Roy Carrillo', desc: "hello this is a placeholder"}] 
+let list = [
+    {
+        id: 1,
+        title: "School Board",
+        image: "https://placehold.co/600x400",
+        author: "Javier Carrillo",
+        desc: "hello this is a placeholder",
+    },
+    {
+        id: 2,
+        title: "Congrats",
+        image: "https://placehold.co/600x400",
+        author: "Roy Carrillo",
+        desc: "hello this is a placeholder",
+    },
+];
 
 const BoardDisplay = () => {
     return (
         <div className="board-container">
-            {placeholderList.map(board => <Board board={board}/>)}
+            {list.length ? (
+                list.map((board) => <Board board={board} />)
+            ) : (
+                <WelcomeBoard />
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default BoardDisplay;
---
+++ frontend/src/components/WelcomeBoard.jsx
@@ -0,0 +1,12 @@
+const WelcomeBoard = () => {
+    return (
+        <div className="board">
+            <h2>"Welcome to Kudomate!"</h2>
+            <img src="./snoopy.gif" alt="Welcome board image"></img>
+            <h3>Javi</h3>
+            <p>To start, press the create board button to the left!</p>
+        </div>
+    )
+}
+
+export default WelcomeBoard;
