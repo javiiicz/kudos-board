d8707a799dad97cb52931e613b6791e0076515f2
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-17 16:05:57 -0700

    [MVP] [Frontend] Add fetch function to get boards on load

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/App.css";
 import Footer from "./Footer";
 import { Route, Routes } from "react-router-dom";
@@ -6,10 +6,35 @@ import HomePage from "./HomePage.jsx";
 import BoardPage from "./BoardPage.jsx";
 
 function App() {
+    const [boards, setBoards] = useState([]);
+
+    const fetchGET = async (url) => {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Response is not ok");
+        }
+        const body = await response.json();
+        return body;
+    };
+
+    const fetchBoards = async () => {
+        let fetchedBoards = []
+        try {
+            fetchedBoards = await fetchGET("http://localhost:3000/boards");
+        } catch (e) {
+            console.log('No boards found...')
+        } 
+        setBoards(fetchedBoards);
+    };
+
+    useEffect(() => {
+        fetchBoards();
+    }, []);
+
     return (
         <>
             <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path="/" element={<HomePage boards={boards} />} />
                 <Route path="/boards/:id" element={<BoardPage />} />
             </Routes>
             <Footer />
