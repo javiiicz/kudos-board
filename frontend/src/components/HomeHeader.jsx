baafe06aeea09bf945baa8f883d7d58bab4cacd4
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 17:07:29 -0700

    [MVP] [Frontend] Abstract Title heading

--- frontend/src/components/HomeHeader.jsx
+++ frontend/src/components/HomeHeader.jsx
@@ -1,17 +1,18 @@
-import "../styles/Header.css"
+import "../styles/HomeHeader.css";
 import Search from "./Search";
 import Filters from "./Filters";
 import CreateButton from "./CreateButton";
+import Title from "./Title";
 
 const HomeHeader = () => {
     return (
         <header>
-            <h1>Kudos Board!</h1>
-            <Search/>
-            <Filters/>
-            <CreateButton/>
+            <Title/>
+            <Search />
+            <Filters />
+            <CreateButton />
         </header>
-    )
-}
+    );
+};
 
 export default HomeHeader;
---
+++ frontend/src/components/Title.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import "../styles/Title.css"
+
+const Title = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/');
+    }
+
+    return (
+        <button onClick={handleClick} className="title-button">
+            <h1>Kudos Board!</h1>
+        </button>
+    );
+};
+
+export default Title;
