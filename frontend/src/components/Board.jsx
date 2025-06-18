c541d7ade66c94ca7eb92728df602cf5dcae38a8
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-17 23:01:42 -0700

    [MVP] [Frontend] Delete extra info shown on board

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -12,8 +12,6 @@ const Board = ({board}) => {
         <div className="board" onClick={handleBoardClick}>
             <h2>{board.title}</h2>
             <img src={board.imageUrl} alt={board.title} className="board-image"></img>
-            <h3 className="author">By {board.author}</h3>
-            <p className="board-desc">{board.description}</p>
             <div className="category">
                 <p>{board.category}</p>
             </div>
