6c72b4f1734e56de744b3b436a0d5aea3a331198
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-17 22:29:52 -0700

    [MVP] [Frontend] [Styling] Tweak the styling for he boards

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -11,9 +11,12 @@ const Board = ({board}) => {
     return (
         <div className="board" onClick={handleBoardClick}>
             <h2>{board.title}</h2>
-            <img src={board.imageUrl} alt={board.title}></img>
-            <h3>{board.author}</h3>
-            <p>{board.description}</p>
+            <img src={board.imageUrl} alt={board.title} className="board-image"></img>
+            <h3 className="author">By {board.author}</h3>
+            <p className="board-desc">{board.description}</p>
+            <div className="category">
+                <p>{board.category}</p>
+            </div>
         </div>
     )
 }
