b75991aa81f9f5570741e4af8ed08cc11c309da6
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-18 11:08:30 -0700

    [MVP] [Frontend] Change error header

---
+++ frontend/src/components/ErrorHeader.jsx
@@ -0,0 +1,12 @@
+import "../styles/HomeHeader.css";
+import Title from "./Title";
+
+const ErrorHeader = () => {
+    return (
+        <header>
+            <Title/>
+        </header>
+    );
+};
+
+export default ErrorHeader;
--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -1,10 +1,10 @@
-import HomeHeader from "./HomeHeader.jsx";
+import ErrorHeader from "./ErrorHeader.jsx";
 import "../styles/ErrorPage.css"
 
 const ErrorPage = ({error}) => {
     return (
         <>
-            <HomeHeader />
+            <ErrorHeader />
             <main className="error-main">
                 <h2>Oops, something went wrong</h2>
                 <p>{error ? error.message : "The page does not exist"}</p>
