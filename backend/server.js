405a823cea9981996cf266bf659e401596a206f8
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-18 11:37:20 -0700

    [MVP] [Backend] Add PATCH endpoint for card upvotes

--- backend/server.js
+++ backend/server.js
@@ -1,79 +1,81 @@
 const express = require("express");
 const cors = require("cors");
-const { PrismaClient } = require('./generated/prisma')
-const prisma = new PrismaClient()
-
+const { PrismaClient } = require("./generated/prisma");
+const prisma = new PrismaClient();
 
 const server = express();
-server.use(cors())
+server.use(cors());
 server.use(express.json());
 
 // [GET] all boards
 server.get("/boards", async (req, res) => {
-    
-    let boards = await prisma.board.findMany()
+    let boards = await prisma.board.findMany();
 
     if (!boards.length) {
-        next({message: "No boards were found", status: 404})
-        return
+        next({ message: "No boards were found", status: 404 });
+        return;
     }
 
-    res.json(boards)
-})
-
+    res.json(boards);
+});
 
 // [GET] board by id
 server.get("/boards/:id", async (req, res, next) => {
-    
-    let id = req.params.id
+    let id = req.params.id;
 
     // make sure id is Integer
     if (isNaN(id)) {
-        next({message: "ID of the board has to be an integer", status: 400})
-        return
+        next({ message: "ID of the board has to be an integer", status: 400 });
+        return;
     }
 
-    let board = await prisma.board.findUnique({where: {id: parseInt(id)}});
+    let board = await prisma.board.findUnique({ where: { id: parseInt(id) } });
 
     if (!board) {
-        next({message: "The board with the specified ID does not exist", status: 404})
+        next({
+            message: "The board with the specified ID does not exist",
+            status: 404,
+        });
     } else {
         res.json(board);
     }
-})
-
+});
 
 // [GET] cards for a specific board
-server.get("/boards/:boardID/cards", async(req,res,next) => {
-    let id = req.params.boardID
+server.get("/boards/:boardID/cards", async (req, res, next) => {
+    let id = req.params.boardID;
 
     // make sure id is Integer
     if (isNaN(id)) {
-        next({message: "ID of the board has to be an integer", status: 400})
-        return
+        next({ message: "ID of the board has to be an integer", status: 400 });
+        return;
     }
 
-    let cards = await prisma.card.findMany({where: {boardId: parseInt(id)}});
+    let cards = await prisma.card.findMany({
+        where: { boardId: parseInt(id) },
+    });
 
     res.json(cards);
-})
-
+});
 
 // [POST] new board
 server.post("/boards", async (req, res, next) => {
     let body = req.body;
 
     if (body === undefined) {
-        next({message: "The request has no body", status: 400})
-        return
+        next({ message: "The request has no body", status: 400 });
+        return;
     }
 
-    let {title, imageUrl, category, author} = body
+    let { title, imageUrl, category, author } = body;
 
-
-    if (title === undefined ||  imageUrl === undefined || category === undefined) {
-        next({message: "The new board is missing information", status: 400})
-        return
+    if (
+        title === undefined ||
+        imageUrl === undefined ||
+        category === undefined
+    ) {
+        next({ message: "The new board is missing information", status: 400 });
+        return;
     }
 
     const added = await prisma.board.create({
@@ -81,34 +83,41 @@ server.post("/boards", async (req, res, next) => {
             title,
             imageUrl,
             category,
-            author
-        }
-    })
+            author,
+        },
+    });
 
     res.json(added);
-})
-
+});
 
 // [POST] new card
 server.post("/boards/:boardID/cards", async (req, res, next) => {
     let body = req.body;
 
-    let boardId = req.params.boardID
-    let {title, description, gifUrl, author} = body
+    let boardId = req.params.boardID;
+    let { title, description, gifUrl, author } = body;
 
-
-    if (title === undefined ||  description === undefined || gifUrl === undefined || isNaN(boardId)) {
-        next({message: "The new card is missing information", status: 400})
-        return
+    if (
+        title === undefined ||
+        description === undefined ||
+        gifUrl === undefined ||
+        isNaN(boardId)
+    ) {
+        next({ message: "The new card is missing information", status: 400 });
+        return;
     }
 
-    boardId = parseInt(boardId)
+    boardId = parseInt(boardId);
 
     // Check that the board exists
-    const exists = (await prisma.board.findUnique({where: {id: boardId}})) !== null
+    const exists =
+        (await prisma.board.findUnique({ where: { id: boardId } })) !== null;
     if (!exists) {
-        next({message: "The board where you are posting this card does not exist", status: 400})
-        return
+        next({
+            message: "The board where you are posting this card does not exist",
+            status: 400,
+        });
+        return;
     }
 
     const added = await prisma.card.create({
@@ -117,66 +126,122 @@ server.post("/boards/:boardID/cards", async (req, res, next) => {
             description,
             gifUrl,
             author,
-            boardId
-        }
-    })
+            boardId,
+        },
+    });
 
     res.json(added);
-})
+});
 
 // [DELETE] a board
 server.delete("/boards/:id", async (req, res, next) => {
-    let id = req.params.id
+    let id = req.params.id;
 
     // make sure id is Integer
     if (isNaN(id)) {
-        next({message: "ID of the board has to be an integer", status: 400})
-        return
+        next({ message: "ID of the board has to be an integer", status: 400 });
+        return;
     }
 
-    id = parseInt(id)
+    id = parseInt(id);
 
     // check if card exists
-    const exists = (await prisma.board.findUnique({where: {id: id}})) !== null
+    const exists =
+        (await prisma.board.findUnique({ where: { id: id } })) !== null;
     if (!exists) {
-        next({message: "The board you are deleting does not exist", status: 404})
-        return
+        next({
+            message: "The board you are deleting does not exist",
+            status: 404,
+        });
+        return;
     }
 
     const deleted = await prisma.board.delete({
-        where: {id: id}
-    })
+        where: { id: id },
+    });
 
-    res.json(deleted)
+    res.json(deleted);
 });
-
 
 // [DELETE] a card
 server.delete("/cards/:id", async (req, res, next) => {
-    let id = req.params.id
+    let id = req.params.id;
 
     // make sure id is Integer
     if (isNaN(id)) {
-        next({message: "ID of the card has to be an integer", status: 400})
-        return
+        next({ message: "ID of the card has to be an integer", status: 400 });
+        return;
     }
 
-    id = parseInt(id)
+    id = parseInt(id);
 
     // check if card exists
-    const exists = (await prisma.card.findUnique({where: {id: id}})) !== null
+    const exists =
+        (await prisma.card.findUnique({ where: { id: id } })) !== null;
     if (!exists) {
-        next({message: "The card you are deleting does not exist", status: 404})
-        return
+        next({
+            message: "The card you are deleting does not exist",
+            status: 404,
+        });
+        return;
     }
 
     const deleted = await prisma.card.delete({
-        where: {id: id}
-    })
+        where: { id: id },
+    });
 
-    res.json(deleted)
+    res.json(deleted);
 });
 
+// [PATCH] a card to like / unlike
+server.patch("/cards/:id", async (req, res, next) => {
+    let id = req.params.id;
+
+    // make sure id is Integer
+    if (isNaN(id)) {
+        next({ message: "ID of the card has to be an integer", status: 400 });
+        return;
+    }
+
+    id = parseInt(id);
+
+    let card = await prisma.card.findUnique({ where: { id: id } });
+
+    // check if card exists
+    const exists = card !== null;
+    if (!exists) {
+        next({
+            message: "The card you are deleting does not exist",
+            status: 404,
+        });
+        return;
+    }
+
+    let updateUser = null;
+    if (card.liked) {
+        updateUser = await prisma.user.update({
+            where: {
+                id: id,
+            },
+            data: {
+                upvotes: card.upvotes - 1,
+                liked: false,
+            },
+        });
+    } else {
+        updateUser = await prisma.user.update({
+            where: {
+                id: id,
+            },
+            data: {
+                upvotes: card.upvotes + 1,
+                liked: true,
+            },
+        });
+    }
+
+    res.json(updateUser);
+});
 
 // [CATCH-ALL]
 server.use((req, res, next) => {
