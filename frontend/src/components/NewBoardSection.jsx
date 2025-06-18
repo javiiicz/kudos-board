56c27bd1a663d52c36f6bdd71225ee7c8352765e
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-17 22:58:55 -0700

    [MVP] [Frontend] Create Component to create a new Board

---
+++ frontend/src/components/NewBoardSection.jsx
@@ -0,0 +1,61 @@
+import {X} from 'lucide-react'
+import "../styles/NewBoardSection.css"
+
+const NewBoardSection = ({setShowModal}) => {
+    return (
+        <div className="modal-container">
+                <X
+                    onClick={() => {
+                        setShowModal(false);
+                    }}
+                    size={30}
+                    className="close-button"
+                />
+                <div className="form-container">
+                    <h2>Create a new Board:</h2>
+                    <form className="add-form">
+                        <label for="title">Title*</label>
+                        <input
+                            type="text"
+                            id="title"
+                            name="title"
+                            required
+                        ></input>
+
+                        <label for="author">Author</label>
+                        <input type="text" id="author" name="author"></input>
+
+                        <label for="image">Image URL*</label>
+                        <input
+                            type="text"
+                            id="image"
+                            name="image"
+                            required
+                        ></input>
+
+                        <label for="description">Description*</label>
+                        <input
+                            type="text"
+                            id="desc"
+                            name="desc"
+                            required
+                        ></input>
+
+                        <label for="category">Category*</label>
+                        <select id="category" name="category" required>
+                            <option value="">Select a category</option>
+                            <option value="Celebration">Celebration</option>
+                            <option value="Inspiration">Inspiration</option>
+                            <option value="Thank You">Thank You</option>
+                        </select>
+
+                        <p>(*) indicates required fields</p>
+
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+    )
+}
+
+export default NewBoardSection;
