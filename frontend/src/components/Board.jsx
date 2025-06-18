14c98295e701531f335a3469113dcca774b46793
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-18 11:16:33 -0700

    [MVP] [Frontend] Create delete button for boards

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,8 +1,8 @@
-import "../styles/Board.css"
+import "../styles/Board.css";
 import { useNavigate } from "react-router-dom";
 
-const Board = ({board}) => {
-    const navigate = useNavigate()
+const Board = ({ board }) => {
+    const navigate = useNavigate();
 
     const handleBoardClick = () => {
         navigate(`/boards/${board.id}`);
@@ -11,12 +11,26 @@ const Board = ({board}) => {
     return (
         <div className="board" onClick={handleBoardClick}>
             <h2>{board.title}</h2>
-            <img src={board.imageUrl} alt={board.title} className="board-image"></img>
+            <img
+                src={board.imageUrl}
+                alt={board.title}
+                className="board-image"
+            ></img>
             <div className="category">
                 <p>{board.category}</p>
             </div>
+            <div className="board-button-container">
+                <button
+                    className="card-button trash"
+                    onClick={() => {
+                        deleteCard(card.id);
+                    }}
+                >
+                    🗑️
+                </button>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Board;
