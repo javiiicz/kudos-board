02408616b5c46384b5f1d7030ff1c31a9396bdcc
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-17 17:00:29 -0700

    [MVP] [Frontend] Add current board details to the page

--- frontend/src/components/BoardDetails.jsx
+++ frontend/src/components/BoardDetails.jsx
@@ -1,10 +1,13 @@
 import Card from "./Card";
 import "../styles/BoardDetails.css"
 
-const BoardDetails = ({id, cards}) => {
+const BoardDetails = ({id, cards, currentBoard}) => {
     return (
         <>
-            <h2>ID = {id}</h2>
+            <h2>{currentBoard.title}</h2>
+            {currentBoard.author && <h3>{currentBoard.author}</h3>}
+            <p>{currentBoard.description}</p>
+            <p>{currentBoard.category}</p>
             <div className="card-container">
                 {cards.map(card => <Card key={card.id} card={card}/>)}
             </div>
